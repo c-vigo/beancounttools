7e7b3929ae1b076166414fb3cf5bcc521f5416d5
Carlos Vigo <carviher1990@gmail.com>
2024-06-06 11:57:44 +0200

    add importer for SplitWise trip ledgers

--- src/tariochbctools/importers/splitters/importer.py
+++ src/tariochbctools/importers/splitters/importer.py
@@ -182,3 +182,124 @@ class HouseHoldSplitWiseImporter(identifier.IdentifyMixin, importer.ImporterProt
                     ))
 
         return entries
+
+
+class TripSplitWiseImporter(identifier.IdentifyMixin, importer.ImporterProtocol):
+    """An importer for SplitWise CSV files."""
+    def __init__(self, regexps, account, owner: str, expenses_account: str, tag: str = None):
+        identifier.IdentifyMixin.__init__(self, matchers=[("filename", regexps)])
+        self.account = account
+        self.owner = owner
+        self.expenses_account = expenses_account
+        self.tag = {tag} if tag is not None else data.EMPTY_SET
+        self.fixme_account = 'Expenses:FIXME'
+
+    def name(self):
+        return super().name() + self.account
+
+    def file_account(self, file):
+        return self.account
+
+    def extract(self, file, existing_entries=None):
+        entries = []
+
+        # Read the CSV file
+        with open(str(file.name), 'r') as csvfile:
+            reader = csv.reader(
+                csvfile,
+                delimiter=","
+            )
+            rows = list(reader)
+
+        # First row: header, sanity checks
+        people = rows[0][5:]
+        if len(people) < 2:
+            raise RuntimeError('Trip Splitwise requires at least two people')
+
+        if self.owner not in people:
+            raise RuntimeError('owner not found in the group')
+
+        idx_owner = people.index(self.owner)
+
+        logging.debug('SplitWise Importer: owner found in pos. {} - {}'.format(idx_owner, people[idx_owner]))
+
+        # Loop over transactions
+        for index, row in enumerate(rows[2:]):
+            # Split fields
+            try:
+                date, description, category, cost, currency, *splits = tuple(row)
+            except ValueError:
+                continue
+
+            # Parse date
+            date = datetime.strptime(date, "%Y-%m-%d").date()
+
+            # Balance?
+            if description == 'Total balance':
+                entries.append(data.Balance(
+                    data.new_metadata(file.name, index),
+                    date,
+                    self.account,
+                    amount.Amount(clean_decimal(splits[idx_owner]), currency),
+                    None,
+                    None
+                ))
+
+            else:
+                # Parse fields
+                splits = [clean_decimal(split) for split in splits]
+                cost = clean_decimal(cost)
+                owner_balance = splits[idx_owner]
+                others_balance = sum(splits) - owner_balance
+                all_zeroes = all([split == D(0) for split in splits])
+
+                # Case 1: no liability for anyone, fully paid by owner and just tracked here
+                if all_zeroes:
+                    continue
+                    entries.append(data.Transaction(
+                        data.new_metadata(file.name, index, {'category': category}),
+                        date,
+                        "*",
+                        self.owner,
+                        description,
+                        self.tag,
+                        data.EMPTY_SET,
+                        [
+                            data.Posting(self.expenses_account, amount.Amount(cost, currency), None, None, None, None)
+                        ],
+                    ))
+                # Case 2: owner not ivolved, paid and owed by others
+                elif owner_balance  == D(0) and others_balance == D(0):
+                    continue
+                # Case 3: negative balance for owner
+                elif owner_balance < D(0):
+                    entries.append(data.Transaction(
+                        data.new_metadata(file.name, index, {'category': category}),
+                        date,
+                        "*",
+                        "",
+                        description,
+                        self.tag,
+                        data.EMPTY_SET,
+                        [
+                            data.Posting(self.account, amount.Amount(owner_balance, currency), None, None, None, None),
+                            data.Posting(self.expenses_account, amount.Amount(-owner_balance, currency), None, None, None, None)
+                        ],
+                    ))
+                # Case 4: positive balance for owner
+                else:
+                    entries.append(data.Transaction(
+                        data.new_metadata(file.name, index, {'category': category}),
+                        date,
+                        "*",
+                        "",
+                        description,
+                        self.tag,
+                        data.EMPTY_SET,
+                        [
+                            data.Posting(self.account, amount.Amount(owner_balance, currency), None, None, None, None),
+                            data.Posting(self.expenses_account, amount.Amount(cost-owner_balance, currency), None, None, None, None)
+                        ],
+                    ))
+
+        return entries
