baeaecbd4deebacfec708a5124e2038febcc24f1
Carlos Vigo <carviher1990@gmail.com>
2025-04-04 09:22:14 +0200

    add support for ZKB CSV files

--- src/tariochbctools/importers/zkb/importer.py
+++ src/tariochbctools/importers/zkb/importer.py
@@ -38,3 +38,85 @@ class ZkbImporter(mt940importer.Importer):
             narration = details.strip()
 
         return narration
+
+
+import csv
+
+from beancount.core import amount, data
+from beancount.core.number import D
+from beancount.ingest import importer
+from beancount.ingest.importers.mixins import identifier
+from datetime import datetime
+
+
+
+class ZkbCSVImporter(identifier.IdentifyMixin, importer.ImporterProtocol):
+    """An importer for ZKB CSV files."""
+
+    def __init__(self, regexps, account):
+        identifier.IdentifyMixin.__init__(self, matchers=[("filename", regexps)])
+        self.account = account
+
+    def name(self):
+        return super().name() + self.account
+
+    def file_account(self, file):
+        return self.account
+
+    def extract(self, file, existing_entries):
+        entries = []
+
+        with open(file.name, 'r', encoding='utf8') as csvfile:
+            reader = csv.DictReader(
+                csvfile,
+                [
+                    "Date",
+                    "Booking text",
+                    "Curr",
+                    "Amount details",
+                    "ZKB reference",
+                    "Reference number",
+                    "Debit CHF",
+                    "Credit CHF",
+                    "Value date",
+                    "Balance CHF",
+                    "Payment purpose",
+                    "Details"
+                ],
+                delimiter=";"
+            )
+            rows = list(reader)[1:]
+
+        for index, row in enumerate(rows):
+            try:
+                # Parse transaction
+                meta = data.new_metadata(file.name, index)
+                meta['zkb_reference'] = row['ZKB reference']
+
+                # Parse date with format DD.MM.YYYY
+                book_date = datetime.strptime(row['Date'].strip(), "%d.%m.%Y").date()
+                description = row["Booking text"].strip() if row["Booking text"] else ""
+                
+                currency = row["Curr"].strip() if row["Curr"] else "CHF"
+                if row["Debit CHF"] != "":
+                    cash_flow = amount.Amount(-D(row["Debit CHF"]), currency)
+                else:
+                    cash_flow = amount.Amount(D(row["Credit CHF"]), currency)
+
+                entries.append(data.Transaction(
+                    meta,
+                    book_date,
+                    "*",
+                    "",
+                    description,
+                    data.EMPTY_SET,
+                    data.EMPTY_SET,
+                    [
+                        data.Posting(self.account, cash_flow, None, None, None, None),
+                    ],
+                ))
+
+            except BaseException as e:
+                raise Warning('Error parsing line {}\n{} from file {}'.format(row, e, file.name))
+
+        return entries
